c028b4b7445e74e631d62f8a3ddd70f26df84a74
Stephen Panzer <radicaledward@gmail.com>
2018-05-18 17:54:33 -0600

    Delete button for movies

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -9,6 +9,12 @@ export default Controller.extend({
         addMovie(){
             let newMovie = this.get('store').createRecord('movie',{title:this.title, yearReleased:this.yearReleased, length:this.length});
             newMovie.save();
+        },
+        deleteMovie(id){
+            this.get('store').findRecord('movie', id).then(function(movie){
+                movie.deleteRecord();
+                movie.save();
+            })
         }
     }
 });
