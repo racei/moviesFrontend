853224cd1ef9feaf02b448f6b308d588c08dd63f
Stephen Panzer <radicaledward@gmail.com>
2018-05-18 17:01:06 -0600

    Switch to ember data from the raw JSON. Added models for movie and user

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -2,6 +2,10 @@ import Route from '@ember/routing/route';
 import $ from 'jquery';
 export default Route.extend({
     model(){
-        return $.get("http://localhost:59585/api/Movies");
-    }
+        //return $.get("http://localhost:59585/api/Movies");
+        return this.get('store').findAll('movie');
+    },
+    title: '',
+    yearReleased: '',
+    length: ''
 });
--- app/routes/users.js
+++ app/routes/users.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(){
-        return $.get('http://localhost:59585/api/Users')
+        return this.get('store').findAll('user');
     }
 });
