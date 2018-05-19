b639883cbdc2447af8c50670762435d7e2fc6cbd
Stephen Panzer <radicaledward@gmail.com>
2018-05-19 17:38:18 -0600

    Add relationship between movie and user in ember model

--- app/models/movie.js
+++ app/models/movie.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     title: DS.attr('string'),
     yearReleased: DS.attr('number'),
-    length: DS.attr('string')
+    length: DS.attr('string'),
+    tags: DS.hasMany('user')
 
 });
