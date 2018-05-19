35560efd871aba88e79ffb2a6471093cf4eeb3a3
Stephen Panzer <radicaledward@gmail.com>
2018-05-19 17:14:15 -0600

    Put user data in a table

--- app/components/user-listing.js
+++ app/components/user-listing.js
@@ -4,6 +4,7 @@ import {inject as service} from '@ember/service';
 export default Component.extend({
     name: '',
     editing: false,
+    tagName: 'tr',
     store: service(),
     actions:{
         deleteUser(id){
