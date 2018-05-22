b26995e91007f077516f410dbc3d38db270b2f5d
Stephen Panzer <radicaledward@gmail.com>
2018-05-22 14:04:25 -0600

    Add both movies and users to model

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -1,6 +1,14 @@
 import Route from '@ember/routing/route';
 export default Route.extend({
-    model(){
-        return this.get('store').findAll('movie');
-    },
+    model() {
+        return Ember.RSVP.hash({
+          movies: this.store.findAll('movie'),
+          users: this.store.findAll('user'),
+        });
+      },
+    
+      setupController(controller, models) {
+        controller.set('movies', models.movies);
+        controller.set('users', models.users);
+      }
 });
