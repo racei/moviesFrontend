7bc79517c4b22479b325cbe956bc2c3e573eb3e8
Stephen Panzer <radicaledward@gmail.com>
2018-05-22 16:47:56 -0600

    Bootstrap stuff for the user page

--- app/components/user-listing.js
+++ app/components/user-listing.js
@@ -4,7 +4,6 @@ import {inject as service} from '@ember/service';
 export default Component.extend({
     name: '',
     editing: false,
-    tagName: 'tr',
     store: service(),
     actions:{
         deleteUser(id){
