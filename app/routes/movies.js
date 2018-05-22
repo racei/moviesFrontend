5b4a3766a5b8776235491e48fbc9f91e9fb5993f
Stephen Panzer <radicaledward@gmail.com>
2018-05-22 14:32:42 -0600

    Change as suggested by the ember linter

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
+import {hash} from 'rsvp';
 export default Route.extend({
     model() {
-        return Ember.RSVP.hash({
+        return hash({
           movies: this.store.findAll('movie'),
           users: this.store.findAll('user'),
         });
