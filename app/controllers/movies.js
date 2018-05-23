b36046b7589713d49bfa7a351b8d7653612c001c
Stephen Panzer <radicaledward@gmail.com>
2018-05-22 18:05:13 -0600

    Simple validation for adding a movie

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -5,8 +5,30 @@ export default Controller.extend({
     title: '',
     yearReleased: '',
     length: '',
+    init() {
+        this._super(...arguments);
+        this.errors = [];
+    },
     actions:{
         addMovie(){
+            this.set('errors', []);
+            let newErrors = [];
+            if(this.title.length == 0){
+                newErrors.push( "Title must be longer than 0 characters");
+            }
+            let regexLength = /^(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)$/;
+            if(!regexLength.exec(this.length)){
+                newErrors.push( "Movie length must be in HH:MM:SS format");
+            }
+            let regexYear = /^\d{4}$/;
+            if(!regexYear.exec(this.yearReleased)){
+                newErrors.push("Year must be entered");
+            }
+            if(newErrors != []){
+                this.set('errors', newErrors);
+                return;
+            }
+
             let newMovie = this.get('store').createRecord('movie',{title:this.title, yearReleased:this.yearReleased, length:this.length});
             this.set('title', '');
             this.set('yearReleased', '');
